6f82248ac7b863b033c2f25d17d392363789732c
WendyWiranata <wen2wiranata@gmail.com>
2025-06-11 20:03:26 +0700

    Memperbaiki error migrasi dan permission

--- sap-system/database/seeders/RoleSeeder.php
+++ sap-system/database/seeders/RoleSeeder.php
@@ -11,9 +11,26 @@ class RoleSeeder extends Seeder
     public function run()
     {
         // Reset cached roles and permissions
-        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+    app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
        // Buat permission hanya jika belum ada
+          $permissions = ['company-crud', 'admin-crud', 'employee-crud'];
+    foreach ($permissions as $perm) {
+        Permission::firstOrCreate(['name' => $perm, 'guard_name' => 'web']);
+    }
+      // Buat role SuperAdmin
+    $role = Role::firstOrCreate(['name' => 'SuperAdmin', 'guard_name' => 'web']);
+    $role->givePermissionTo(Permission::all());
+
+    // Buat user SuperAdmin
+    User::firstOrCreate([
+        'email' => 'superadmin@example.com'
+    ], [
+        'name' => 'Super Admin',
+        'password' => bcrypt('password'),
+        'company_id' => 1
+    ])->assignRole('SuperAdmin');
+    
         Permission::firstOrCreate(['name' => 'company-crud', 'guard_name' => 'web']);
         Permission::firstOrCreate(['name' => 'admin-crud', 'guard_name' => 'web']);
         $permissions = [
